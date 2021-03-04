51e514e7b2a528c4a4debd26f20313e7db9c8e68
megiakhalkatsi <megi.akhalkatsi@gmail.com>
2021-03-04 12:38:10 +0400

    add login page

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -83,4 +83,15 @@ const selectDropdown = () => {
 }
 selectDropdown()
 
+// password show function
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    // this.classList.toggle('fa-eye-slash');
+});
 //# sourceMappingURL=main.js.map
