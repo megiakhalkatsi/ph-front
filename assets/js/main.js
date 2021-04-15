c515e56f6644626714111564fbd2065b73fe44ab
megiakhalkatsi <megi.akhalkatsi@gmail.com>
2021-04-15 23:34:04 +0400

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,7 @@ function burgerMenuResponsive(x) {
 
 //full size images
 var imgs = document.getElementsByClassName("resource__item__img");
+
 for (var i = 0; i < imgs.length; i++) {
     imgs[i].onclick = function () { imageZoom(this); }
 }
@@ -208,4 +209,244 @@ togglePassword.addEventListener('click', function (e) {
         })
 })()
 
+
+var questions = [
+  {
+    number: 1,
+    correctAnswer: 1,
+    question: 'საქართველო არის სამართლებრივი სახელმწიფო, რაც ნიშნავს, რომ ხელისუფლება ხორციელდება....',
+    answers: [
+      {
+        isCorrect: true,
+        text: "საქართველოს კონსტიტუციითა და კანონით დადგენილ ფარგლებში"
+      },
+      {
+        isCorrect: false,
+        text: "უძველესი ჩვეულებებისა და ადათ-წესების შესაბამისად"
+      },
+      {
+        isCorrect: false,
+        text: "გამორჩეული ადამიანების ნება-სურვილის მიხედვით"
+      },
+      {
+        isCorrect: false,
+        text: "უცხო ქვეყნების გადაწყვეტილებებით"
+      },
+    ]
+  },
+  {
+    number: 2,
+    correctAnswer: 1,
+    question: 'სახელმწიფო ცნობს და იცავს......',
+    answers: [
+      {
+        isCorrect: true,
+        text: "ადამიანის საყოველთაოდ აღიარებულ უფლებებსა და თავისუფლებებს"
+      },
+      {
+        isCorrect: false,
+        text: "მხოლოდ სამშობლოს წინაშე დიდი დამსახურების მქონე ადამიანების უფლებებსა და ნება-სურვილს"
+      },
+      {
+        isCorrect: false,
+        text: "მხოლოდ ქართველების უფლებებსა და თავისუფლებებს"
+      },
+      {
+        isCorrect: false,
+        text: "იმ ადამიანების უფლებებს, რომლებსაც  წაკითხული აქვთ  კონსტიტუცია"
+      },
+    ]
+  },
+  {
+    number: 3,
+    correctAnswer: 1,
+    question: 'საქართველოში ყველა ადამიანი სამართლის წინაშე…',
+    answers: [
+      {
+        isCorrect: true,
+        text: "თანასწორია"
+      },
+      {
+        isCorrect: false,
+        text: "თანასწორია, მაგრამ კანონი უშვებს გამონაკლის შემთხვევებსაც"
+      },
+      {
+        isCorrect: false,
+        text: "თანასწორია, თუმცა შეიძლება პრივილეგიის ყიდვა ან დამსახურებით მოპოვება"
+      },
+      {
+        isCorrect: false,
+        text: "უთანასწოროა"
+      },
+    ]
+  },
+  {
+    number: 4,
+    correctAnswer: 1,
+    question: 'აკრძალულია ადამიანის...',
+    answers: [
+      {
+        isCorrect: true,
+        text: "დისკრიმინაცია მისი რასის, კანის ფერის, სქესის, წარმოშობის ან სხვა რაიმე მისთვის დამახასიათებელი ნიშან-თვისების მიხედვით"
+      },
+      {
+        isCorrect: false,
+        text: "თანასწორად მოპყრობა, თუკი იგი ამას არ იმსახურებს"
+      },
+      {
+        isCorrect: false,
+        text: "მონაწილეობა კანონების შექმნის პროცესში"
+      },
+      {
+        isCorrect: false,
+        text: "მონაწილეობა ქვეყნის მმართველების არჩევაში"
+      },
+    ]
+  },
+]
+
+var index = 0;
+
+
+
+
+function getCorrectAnsweersMarkup(correctAnswer){
+  console.log(correctAnswer)
+  $('#ProgressBar').attr('style', `--v: ${correctAnswer * 45}deg`);
+  $('#ProgressText').html(`კითხვა <span class="purple">${correctAnswer}</span> / ${questions.length} დან`)
+
+}
+
+
+function getQuestionsMarkup(index){
+
+  const questionMarkup = questions.map(q => (
+    `<div class="col-12 col-lg-8 page__content-col m-auto ${q.number} questionContainer" style="display: ${index + 1 === q.number ? "block" : "none"}">
+      <h2 class="game__text lg mb-34 GameQuestion">${q.number}. ${q.question}</h2>
+      
+      <div class="game__answer__drop__container DroppableItem" data-append="false">
+        <p class="game__answer__drop__container__placeholder">ჩააგდეთ სწორი პასუხი</p>
+      </div>
+      
+      <div class="game__line"></div>
+
+      <h2 class="game__title mb-34">სავარაუდო პასუხები</h2>
+
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <div class="game__answer__container DraggableItem" data-answer="1">
+            <p class="game__text md">საქართველოს კონსტიტუციითა და კანონით დადგენილ ფარგლებში</p>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="game__answer__container DraggableItem" data-answer="2">
+            <p class="game__text md">უძველესი ჩვეულებებისა და ადათ-წესების შესაბამისად </p>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="game__answer__container DraggableItem" data-answer="3">
+            <p class="game__text md">გამორჩეული ადამიანების ნება-სურვილის მიხედვით</p>
+          </div>
+        </div>
+        <div class="col-12 col-lg-6">
+          <div class="game__answer__container DraggableItem" data-answer="4">
+            <p class="game__text md">უცხო ქვეყნების გადაწყვეტილებებით</p>
+          </div>
+        </div>
+      </div>
+      <div class="game__line"></div>
+    </div>`
+  ))
+
+  return questionMarkup
+
+}
+
+
+$( function() {
+  const randomArray = questions.map(array => {
+    var answers = array.answers.sort(function() {return 0.5 - Math.random()})
+
+    return {...array, answers}
+  })
+
+  questions = randomArray
+
+  const items = getQuestionsMarkup(index)
+
+  $('#GameWrapper').append(items)
+});
+
+
+
+$( "#nextStep" ).click(function() {
+  index++
+
+  if(index === questions.length || index > 4 ) {
+    index = 0
+  } 
+  
+  console.log(index)
+
+  $('.questionContainer').attr('style', 'display: none');
+  $('.questionContainer')[index].removeAttribute('style');
+
+});
+
+
+$( "#prevStep" ).click(function() {
+  index--
+  
+  if(index < 0) {
+    index = questions.length -1
+  }
+
+  console.log(index)
+  console.log(questions)
+
+  $('.questionContainer').attr('style', 'display: none');
+  $('.questionContainer')[index].removeAttribute('style');
+});
+
+
+
+$( function() {
+    let incorrect = 0;
+    let attempt = 0;
+    let correctAnswer = 0;
+
+    getCorrectAnsweersMarkup(correctAnswer)
+
+    $( ".DraggableItem" ).draggable({ revert: "invalid" });
+    $( ".DroppableItem" ).droppable({
+      drop: function( event, ui ) {
+          if($( this ).attr('data-append') == 'false' && ui.draggable.attr('data-answer') == 1) {
+            incorrect = 0
+            correctAnswer++
+            $( this ).append(ui.draggable)
+            getCorrectAnsweersMarkup(correctAnswer)
+            $(this).find('.DraggableItem').removeAttr('style')
+            $( this ).attr('data-append', true)
+            $('#DogImage').attr('src', '../assets/img/illustrations/correct-image.gif')
+
+        } else {
+            attempt--
+            if(incorrect < 2) {
+                incorrect++
+
+            } else if(incorrect === 2 || incorrect > 2) {
+              $('#notificationModal').modal('show');
+              $('#DogImage').attr('src', '../assets/img/illustrations/correct-image.gif')
+
+              // $('#myModal').modal('toggle');
+              // $('#myModal').modal('hide');
+            }
+            // console.log(incorrect)
+
+            ui.draggable.attr('style', "position: relative")
+        }
+        attempt++
+      }
+    });
+});
 //# sourceMappingURL=main.js.map
