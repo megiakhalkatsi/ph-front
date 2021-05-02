8b728fe0f2f31ac6bc5c3892ee3d2167c9190a98
Jeko <43757639+front-end-jeko@users.noreply.github.com>
2021-05-02 23:13:01 +0400

    დაემატა თამაში #3

--- src/assets/vendors/js/games/game2.js
+++ src/assets/vendors/js/games/game2.js
@@ -1,1 +1,53 @@
-console.log('render...')
+
+$( function() {
+  $( ".DraggableItem" ).draggable({ revert: "invalid", helper : "clone" });
+  $( ".DroppableItem" ).droppable({
+    drop: function( event, ui ) {
+        if(event.target.classList.contains('game__table__td')) {
+          var new_signature = $(ui.helper).clone();
+          $(this).append(new_signature);
+          $(new_signature).removeAttr('style')
+          $('.game__table__td .game__text').attr('style', 'color: #202631')
+
+          let rowIndex = event.target.parentElement.getAttribute('data-row')
+          let DraggableItemRows = JSON.parse(ui.draggable[0].getAttribute('data-row'))
+          let DraggableItemCollumn = JSON.parse(ui.draggable[0].getAttribute('data-collumn'))
+          let parentIndex = event.target.getAttribute('data-index')
+
+
+          if(DraggableItemRows.includes(rowIndex) && DraggableItemCollumn == parentIndex) {
+              event.target.lastElementChild.setAttribute('data-correct', true)
+          } else {
+              event.target.lastElementChild.setAttribute('data-correct', false)
+          }
+        }
+    }
+  });
+});
+
+
+
+$( "#finishButton" ).click(function() {
+let index = 0
+
+$.each($('.DroppableItem .DraggableItem'), function( i, l ){
+  if($(l).attr('data-correct') == "true") {
+    index++
+    $(l).addClass('success')
+  } else {
+    $(l).addClass('error')
+  }
+
+  if(index === 16) {
+    $('#DogImage').attr('src', '../assets/img/illustrations/correct-image.gif')
+  } else {
+    $('#DogImage').attr('src', '../assets/img/illustrations/incorrect-image.gif')
+  }
+});
+});
+
+
+
+$( "#resetButton" ).click(function() {
+$('.DroppableItem').html('')
+});
