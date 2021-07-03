b5400ccea0a59c00521f7f4f766cc16f2e198662
Jeko <43757639+front-end-jeko@users.noreply.github.com>
2021-07-03 09:38:35 +0400

    complet game4

---
+++ src/assets/vendors/js/games/game4.js
@@ -0,0 +1,42 @@
+
+$(".game__box__bulletsList").focus(function () {
+    if (document.getElementById('game__box__bulletsList').value === '') {
+      document.getElementById('game__box__bulletsList').value += '• ';
+    }
+  });
+
+  $(".game__box__bulletsList").keyup(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+      document.getElementById('game__box__bulletsList').value += '• ' + getElementById('');
+    }
+    var txtval = document.getElementById('game__box__bulletsList').value;
+    if (txtval.substr(txtval.length - 1) == '\n') {
+      document.getElementById('game__box__bulletsList').value = txtval.substring(0, txtval.length - 1);
+    }
+  });
+
+
+
+  $('.game__box__btn').click(function (e) {
+    e.stopPropagation()
+    $(this).addClass('hide');
+    $(this).parent().addClass('active');
+    $(this).prev('.game__box__list__container').addClass('active');
+    $(this).prev('.game__box__list__container').children(".game__box__list").append('<li class="game__box__list__item game__box__item__text"></li>');
+    document.getElementById("editable1").focus();
+  });
+  $(document).on("click", function (e) {
+    if ($(e.target).is(".game__box__list__item") === false) {
+      $('.game__box__btn').removeClass('hide');
+    }
+  });
+  
+
+
+  $(document).ready(function() {
+      $('#downloadBtn').click(() => {
+        window.print()
+    });
+});
+
