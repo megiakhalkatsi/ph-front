666c9844778656e3c252985bd2393d1f04364762
Jeko <43757639+front-end-jeko@users.noreply.github.com>
2021-07-11 00:53:32 +0400

    added game #14

---
+++ src/assets/vendors/js/games/game14.js
@@ -0,0 +1,322 @@
+
+
+
+
+var questions = [
+  {
+    number: 1,
+    correctAnswer: 26,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენი პროცენტის მიმართ გამოიყენეს მშობლებმა/მშობელმა აღზრდის მხოლოდ არაძალადობრივი მეთოდი? ',
+  },
+  {
+    number: 2,
+    correctAnswer: 26,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენ პროცენტს გამოუცდია ფიზიკური დასჯა მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 3,
+    correctAnswer: 5,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენ პროცენტს გამოუცდია მკაცრი ფიზიკური დასჯა მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 4,
+    correctAnswer: 66,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენი პროცენტს გამოუცდია ფსიქოლოგიური აგრესია მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 5,
+    correctAnswer: 85,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება სკოლის სრულ საშუალო საფეხურს (X-XII კლ?',
+  },
+  {
+    number: 6,
+    correctAnswer: 96,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება სკოლის საბაზო საფეხურს (VII-IX კლ)?',
+  },
+  {
+    number: 7,
+    correctAnswer: 98,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება დაწყებით საფეხურს (I-VI კლ)?',
+  },
+  {
+    number: 8,
+    correctAnswer: 6,
+    question: 'ოჯახების რამდენ პროცენტს არ გააჩნია ადეკვატური საშუალებები ხელის დასაბანად წყლის, საპნის ან ხელის დასაბანი ფხვნილის უქონლობის გამო?',
+  },
+  {
+    number: 9,
+    correctAnswer: 27,
+    question: '6-23 თვის ბავშვების რამდენი პროცენტი იღებს საკვები რაციონის მინიმალურ მრავალფეროვნებას და მინიმალური სიხშირით?',
+  },
+  {
+    number: 10,
+    correctAnswer: 14,
+    question: 'კვლევის მომენტისათვის 20-24 წლის გოგონების რამდენი პროცენტი დაქორწინდა 18 წლამდე?',
+  },
+  {
+    number: 11,
+    correctAnswer: 47,
+    question: '18 წლამდე დაქორწინებული 20-24 წლის გოგონების რამდენ პროცენტს აქვს მიღებული მხოლოდ დაწყებითი ან არასრული საშუალო განათლება?',
+  },
+]
+
+
+var questions2 = [
+  {
+    number: 1,
+    correctAnswer: 26,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენი პროცენტის მიმართ გამოიყენეს მშობლებმა/მშობელმა აღზრდის მხოლოდ არაძალადობრივი მეთოდი? ',
+  },
+  {
+    number: 2,
+    correctAnswer: 26,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენ პროცენტს გამოუცდია ფიზიკური დასჯა მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 3,
+    correctAnswer: 5,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენ პროცენტს გამოუცდია მკაცრი ფიზიკური დასჯა მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 4,
+    correctAnswer: 66,
+    question: '1-14 წლამდე ასაკის ბავშვების რამდენი პროცენტს გამოუცდია ფსიქოლოგიური აგრესია მშობლის/მშობლების მხრიდან?',
+  },
+  {
+    number: 5,
+    correctAnswer: 85,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება სკოლის სრულ საშუალო საფეხურს (X-XII კლ?',
+  },
+  {
+    number: 6,
+    correctAnswer: 96,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება სკოლის საბაზო საფეხურს (VII-IX კლ)?',
+  },
+  {
+    number: 7,
+    correctAnswer: 98,
+    question: 'შესაბამისი ასაკის ბავშვების რამდენი პროცენტი სარგებლობს განათლების მიღების უფლებით და ესწრება დაწყებით საფეხურს (I-VI კლ)?',
+  },
+  {
+    number: 8,
+    correctAnswer: 6,
+    question: 'ოჯახების რამდენ პროცენტს არ გააჩნია ადეკვატური საშუალებები ხელის დასაბანად წყლის, საპნის ან ხელის დასაბანი ფხვნილის უქონლობის გამო?',
+  },
+  {
+    number: 9,
+    correctAnswer: 27,
+    question: '6-23 თვის ბავშვების რამდენი პროცენტი იღებს საკვები რაციონის მინიმალურ მრავალფეროვნებას და მინიმალური სიხშირით?',
+  },
+  {
+    number: 10,
+    correctAnswer: 14,
+    question: 'კვლევის მომენტისათვის 20-24 წლის გოგონების რამდენი პროცენტი დაქორწინდა 18 წლამდე?',
+  },
+  {
+    number: 11,
+    correctAnswer: 47,
+    question: '18 წლამდე დაქორწინებული 20-24 წლის გოგონების რამდენ პროცენტს აქვს მიღებული მხოლოდ დაწყებითი ან არასრული საშუალო განათლება?',
+  },
+]
+
+var index = 0;
+var attampt = 0;
+let activeQuestion;
+var correctAnswer;
+var selectedAnswer = 0;
+var correctAnswerCount = 0;
+
+var slider = document.getElementById("myRange");
+var output = document.querySelector(".purple1");
+output.innerHTML = `${slider.value}%`;
+
+slider.oninput = function() {
+  output.innerHTML = `${this.value}%`;
+  selectedAnswer = this.value
+}
+
+
+
+
+$( function() {
+  const randomArray = questions.sort(function() {return 0.5 - Math.random()})
+
+  questions = randomArray
+
+  correctAnswer = randomArray[index].correctAnswer
+  activeQuestion = randomArray[index].number
+  document.querySelector('.game__percent__title').innerText = randomArray[index].question
+});
+
+
+$('#checkAnswer').click(() => {
+    attampt++
+
+    if(correctAnswer === parseInt(selectedAnswer)) {
+        correctAnswerCount++
+        document.querySelector('.purple3').innerText = `${selectedAnswer}%`;
+        document.querySelector('.purple3').setAttribute('style', 'color: #80c78b')
+        document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+
+        let r = questions.map(w => {
+            if(activeQuestion === w.number) {
+                return { ...w, selectAnswer: true, selectedAnswer: selectedAnswer, iscorrect: true }
+            }
+
+            return w
+        })
+
+        questions = r;
+      } else {
+          $('.modalContainer').modal('show');
+          document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+          document.querySelector('.purple3').removeAttribute('style')
+          document.querySelector('.purple3').innerText = `${selectedAnswer}%`;
+          document.querySelector('.modalCorrectAnswer').innerText = `${correctAnswer}%`;
+          document.querySelector('.modalMyAnswer').innerText = `${selectedAnswer}%`
+
+          let r = questions.map(w => {
+              if(activeQuestion === w.number) {
+                  return { ...w, selectAnswer: true, selectedAnswer: selectedAnswer, iscorrect: false }
+              }
+
+              return w
+          })
+
+          questions = r;
+    }
+
+    if(attampt === 11) {
+      document.querySelector('.game__result__container').classList.add('active') 
+      document.querySelector('.game__result__container').setAttribute('style', 'z-index: 111')
+    }
+})
+
+
+
+$( "#nextStep" ).click(function() {
+  index++
+
+  if(index === questions.length || index > 11 ) {
+    index = 0
+  } 
+
+  const items = questions[index]
+  correctAnswer = items.correctAnswer
+  activeQuestion = items.number
+  selectedAnswer = items.selectedAnswer
+  document.querySelector('.game__percent__title').innerText = items.question
+
+  if(items.selectAnswer === true && items.iscorrect == true) {
+      document.querySelector('.purple3').innerText = `${items.correctAnswer}%`;
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+      document.querySelector('.purple3').setAttribute('style', 'color: #80c78b')
+      slider.value = items.selectedAnswer;
+      output.innerHTML = `${items.selectedAnswer}%`;
+
+  } else if(items.selectAnswer === true && items.iscorrect == false) {
+      document.querySelector('.purple3').innerText = `${items.selectedAnswer}%`;
+      document.querySelector('.purple3').removeAttribute('style')
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+
+      slider.value = items.selectedAnswer;
+      output.innerHTML = `${items.selectedAnswer}%`;
+  } else {
+      document.querySelector('.purple3').innerText = '';
+      document.querySelector('.purple3').removeAttribute('style')
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: none')
+      selectedAnswer = 0;
+      slider.value = 0;
+      output.innerHTML = `0%`;
+  }
+});
+
+
+$( "#prevStep" ).click(function() {
+  index--
+  
+  if(index < 0) {
+    index = questions.length -1
+  }
+
+  const items = questions[index]
+  correctAnswer = items.correctAnswer
+  activeQuestion = items.number
+  selectedAnswer = items.selectedAnswer
+  document.querySelector('.game__percent__title').innerText = items.question
+
+  if(items.selectAnswer === true && items.iscorrect == true) {
+      document.querySelector('.purple3').innerText = `${items.correctAnswer}%`;
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+      document.querySelector('.purple3').setAttribute('style', 'color: #80c78b')
+      slider.value = items.selectedAnswer;
+      output.innerHTML = `${items.selectedAnswer}%`;
+
+  } else if(items.selectAnswer === true && items.iscorrect == false) {
+      document.querySelector('.purple3').innerText = `${items.selectedAnswer}%`;
+      document.querySelector('.purple3').removeAttribute('style')
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: block')
+      slider.value = items.selectedAnswer;
+      output.innerHTML = `${items.selectedAnswer}%`;
+
+  } else {
+      document.querySelector('.purple3').innerText = '';
+      document.querySelector('.purple3').removeAttribute('style')
+      document.querySelector('.game__percent__answer').setAttribute('style', 'display: none')
+      selectedAnswer = 0;
+      slider.value = 0;
+      output.innerHTML = `0%`;
+  }
+});
+
+
+
+$( "#successModalBtn" ).click(function() {
+  document.querySelector('.mainBody').setAttribute('style', 'display: none')
+  document.querySelector('.game__result__container').setAttribute('style', 'display: none')
+
+  if(correctAnswerCount >= 6) {
+      $('.successModal').attr('style', 'display: block; position: absolute; top: 0;left: 0;right: 0; z-index: 11111;')
+      document.querySelector('.successModal #correctAnswers').innerHTML = `${correctAnswerCount}/11 კითხვა`
+  }
+  
+  if(correctAnswerCount <= 5) {
+      $('.failModal').attr('style', 'display: block; position: absolute; top: 0;left: 0;right: 0; z-index: 11111;')
+      document.querySelector('.failModal #correctAnswers').innerHTML = `${correctAnswerCount}/11 კითხვა`
+  }
+});
+
+$( "#gameReset" ).click(function() {
+  document.querySelector('.failModal').setAttribute('style', 'display: none')
+  document.querySelector('.successModal').setAttribute('style', 'display: none')
+  document.querySelector('.game__result__container').classList.remove('active')
+
+  document.querySelector('.mainBody').removeAttribute('style')
+  document.querySelector('.game__result__container').removeAttribute('style')
+  document.querySelector('.game__percent__answer').setAttribute('style', 'display: none')
+
+  slider.value = 0;
+
+  const randomArray = questions2.sort(function() {return 0.5 - Math.random()})
+
+  questions = randomArray
+
+  correctAnswer = randomArray[index].correctAnswer
+  activeQuestion = randomArray[index].number
+  document.querySelector('.game__percent__title').innerText = randomArray[index].question
+
+  
+  index = 0;
+  attampt = 0;
+  activeQuestion;
+  correctAnswer;
+  selectedAnswer = 0;
+  correctAnswerCount = 0;
+});
+
+$( ".completGame" ).click(function() {
+  document.querySelector('.mainBody').removeAttribute('style')
+  document.querySelector('.failModal').setAttribute('style', 'display: none')
+  document.querySelector('.successModal').setAttribute('style', 'display: none')
+  document.querySelector('.game__result__container').classList.remove('active')
+});
